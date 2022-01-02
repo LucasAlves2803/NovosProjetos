358b3d480aee599f9e221a7369c01d64f3a42715
LucasAlves2803 <lucascosta2822@gmail.com>
2022-01-02 20:10:31 -0300

    javascript

---
+++ Javacript/script.js
@@ -0,0 +1,35 @@
+console.log("Olá Mundo!!");
+console.log("Esse ano de 2022 não tem para ninguém, eu vou mitar!")
+var x, y;
+
+
+
+
+
+
+function soma(){
+    // console.log(x+y);
+    return x+y;
+}
+
+
+function subtracao(){
+    return x - y;
+}
+
+function multiplicacao(){
+    return x * y;
+}
+
+function divisao(){
+    return x / y;
+}
+
+function ehpar(varr){
+    if (varr % 2 == 0){
+        return 'sim'
+    }
+    else{
+        return 'não'
+    }
+}
